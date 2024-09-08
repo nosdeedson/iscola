7f7bc1fa1df4702fc726ba97a7e6b32f8d7c6d0f
nosdeedson <josemariazumira@gmail.com>
2024-09-08 14:00:49 -0300

    tests repositories

--- src/infrastructure/repositories/class/class.repository.spec.ts
+++ src/infrastructure/repositories/class/class.repository.spec.ts
@@ -2,6 +2,7 @@ import { DomainMocks } from '../../__mocks__/mocks';
 import { AppDataSourceMock } from '../../__mocks__/appDataSourceMock';
 import { ClassRepository} from '../class/class.repository';
 import { ClassEntity } from '../../entities/class/class.entity';
+import { Class } from '../../../domain/class/class';
 
 
 describe('ClassRepository unit test', () => {
@@ -29,7 +30,7 @@ describe('ClassRepository unit test', () => {
         expect(repository).toBeDefined()
     })
 
-    it('shoule save a class on BD', async () =>{
+    it('should save a class on BD', async () =>{
         let schoolGroup = DomainMocks.mockSchoolGroup();
         let classModel = ClassEntity.toClassEntity(schoolGroup);
         let wantedId = schoolGroup.getId();
@@ -40,4 +41,65 @@ describe('ClassRepository unit test', () => {
         
     })
 
+    it('should delete a class on BD', async () =>{
+        let schedule = DomainMocks.mockSchedule();
+        let wantedId = '2ac4ba35-a052-439f-91dc-1a85c655a339';
+        let schoolGroup = new Class('1234', 'nameBook', 'a1', schedule, wantedId);
+        let classModel = ClassEntity.toClassEntity(schoolGroup);
+        await repository.create(classModel);
+        let result = await repository.find(wantedId);
+        expect(result).toBeDefined();
+        expect(await repository.delete(wantedId)).toBe(void 0);
+    })
+
+    it('should find a class on BD', async () =>{
+        let schedule = DomainMocks.mockSchedule();
+        let wantedId = '2ac4ba35-a052-439f-91dc-1a85c655a339';
+        let schoolGroup = new Class('1234', 'nameBook', 'a1', schedule, wantedId);
+        let classModel = ClassEntity.toClassEntity(schoolGroup);
+        await repository.create(classModel);
+        let result = await repository.find(wantedId);
+        expect(result).toBeDefined();
+        expect(result.id).toEqual(wantedId);
+        expect(result.firstDayOfClassInWeek).toEqual(schoolGroup.getSchecule().getDayOfWeek()[0]);
+        expect(result.secondDayOfClassInWeek).toEqual(schoolGroup.getSchecule().getDayOfWeek()[1]);
+    })
+
+    it('should find all class on BD', async () =>{
+        let schedule = DomainMocks.mockSchedule();
+        let schoolGroup = new Class('1234', 'nameBook', 'a1', schedule, '2ac4ba35-a052-439f-91dc-1a85c655a339');
+        let classModel = ClassEntity.toClassEntity(schoolGroup);
+        await repository.create(classModel);
+        let schoolGroup2 = new Class('1234', 'nameBook', 'a1', schedule, '2ac4ba35-a052-439f-91dc-1a85c655a340');
+        let classModel2 = ClassEntity.toClassEntity(schoolGroup2);
+        await repository.create(classModel2);
+
+        let results = await repository.findAll();
+        expect(results).toBeDefined();
+        expect(results.length).toBe(2);
+        expect(results[0].firstDayOfClassInWeek).toEqual(schoolGroup.getSchecule().getDayOfWeek()[0]);
+        expect(results[0].secondDayOfClassInWeek).toEqual(schoolGroup.getSchecule().getDayOfWeek()[1]);
+    })
+
+    it('should update a class on BD', async () =>{
+        let schedule = DomainMocks.mockSchedule();
+        let wantedId = '2ac4ba35-a052-439f-91dc-1a85c655a339';
+        
+        let schoolGroup = new Class('1234', 'nameBook', 'a1', schedule, wantedId);
+        let classModel = ClassEntity.toClassEntity(schoolGroup);
+        await repository.create(classModel);
+        let result = await repository.find(wantedId);
+        expect(result).toBeDefined();
+        let wantedBookName = 'another book';
+        let wantedClassName = 'b1';
+        classModel.bookName = wantedBookName;
+        classModel.className = wantedClassName;
+        await repository.update(classModel, wantedId);
+
+        result = await repository.find(wantedId);
+
+        expect(result.id).toEqual(wantedId);
+        expect(result.bookName).toEqual(wantedBookName);
+        expect(result.className).toEqual(wantedClassName);
+    })
 })
