8f5386928e327d36a6971a8fa9573dac7bba03de
nosdeedson <josemariazumira@gmail.com>
2024-11-16 19:00:17 -0300

    finished parent usecases

--- src/usecases/parent/update/udpate.parent.usecase.unit.test.spec.ts
+++ src/usecases/parent/update/udpate.parent.usecase.unit.test.spec.ts
@@ -2,6 +2,7 @@ import { MockRepositoriesForUnitTest } from '../../../infrastructure/__mocks__/m
 import { DomainMocks } from '../../../infrastructure/__mocks__/mocks';
 import { UpdateParentUseCase } from '../update/update.parent.usecase';
 import { StudentEntity } from '../../../infrastructure/entities/student/student.entity';
+import { ParentEntity } from '../../../infrastructure/entities/parent/parent.entity';
 
 describe('UpdateParentUseCase unit test', () =>{
 
@@ -20,6 +21,18 @@ describe('UpdateParentUseCase unit test', () =>{
         }
     });
 
-    
+    it('given wrong id should throw an systemError', async () =>{
+        const parentRepository = MockRepositoriesForUnitTest.mockRepositories();
+        const parent = DomainMocks.mockParent();
+        const parentEntity = ParentEntity.toParentEntity(parent);
+        parentRepository.find = jest.fn().mockImplementationOnce(() => parentEntity);
+        
+        const usecase = new UpdateParentUseCase(parentRepository);
+        const student = DomainMocks.mockStudent();
+        const studentEntity = StudentEntity.toStudentEntity(student);
+        parentRepository.update = jest.fn().mockImplementationOnce(() => parentEntity.students.push(studentEntity))
+        const parentId = parent.getId()
+        expect(await usecase.execute(studentEntity, parentId)).toBe(void 0);
+    });
 
 })
